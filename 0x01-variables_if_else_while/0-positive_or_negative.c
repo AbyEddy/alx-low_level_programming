c50810a16064263f925ab66c49da39fd7875d0fe
AbyEddy <Abasqueen11@gmail.com>
2022-09-11 21:10:07 -0700

    king

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,14 @@
  */
 int maim(void)
 {
-      int n;
-      strand (time));
-      n  = rand() - RAND_MAX / 2;
-      if (n == 0)
-	printf("%d is zero\n", n);
-        else if (n < 0)
-	  printf("%d is negative\n", n);
-        else 
-	  printf("%d is positive\n", n);
-	return (0)
+ int n;
+ strand (time));
+ n  = rand() - RAND_MAX / 2;
+    if (n == 0)
+printf("%d is zero\n", n);
+    else if (n < 0)
+printf("%d is negative\n", n);
+    else 
+printf("%d is positive\n", n);
+    return (0)
 }		
